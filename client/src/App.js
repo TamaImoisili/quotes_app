7ef198765cfb734427d33666f5b2e45be8064849
TamaImoisili <tamaimoisili2243@gmail.com>
2023-11-25 06:23:00 -0500

    3

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> redacted again two.
+          Edit <code>src/App.js</code> redacted again two 3.
         </p>
         <a
           className="App-link"
