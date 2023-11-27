b46c1f358d9aeabe1160aa91e4de1161c292db83
TamaImoisili <tamaimoisili2243@gmail.com>
2023-11-26 20:03:05 -0500

    removing buttons from quote container

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,17 @@
 import './App.css';
 import QuoteContainer from './Components/QuoteContainer';
+import NewQuoteButton from './Components/NewQuoteButton';
+import FavouritesButton from './Components/FavouritesButton';
 
 function App() {
   const staticQuote = { text:'Climb the mountains and get their good tidings. Nature’s peace will flow into you as sunshine flows into trees. The winds will blow their own freshness into you, and the storms their energy, while cares will drop away from you like the leaves of Autumn.' }
   const staticAuthor = {text: 'John Muir'};
-  //
+  //<QuoteContainer quote={staticQuote} author={staticAuthor.text} />
   return (
     <div className="App">
-      <QuoteContainer quote={staticQuote} author={staticAuthor.text} />
+    <QuoteContainer quote={staticQuote} author={staticAuthor.text} />
+      <NewQuoteButton/>
+      <FavouritesButton/>
     </div>
   );
 }
--- client/src/Components/QuoteContainer.js
+++ client/src/Components/QuoteContainer.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import '../Styles/QuoteContainer.css';
-import NewQuoteButton from './NewQuoteButton';
-import FavouritesButton from './FavouritesButton';
 import { faQuoteLeft } from '@fortawesome/free-solid-svg-icons';
 import { faQuoteRight } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -16,8 +14,6 @@ function QuoteContainer({ quote, author }) {
           <i className="fa-quote-right"> <FontAwesomeIcon icon={faQuoteRight} /></i> 
        </p>
       {author && <p className="QuoteAuthor">{author}.</p>}
-      <NewQuoteButton/>
-      <FavouritesButton/>
     </div>
   );
 }
